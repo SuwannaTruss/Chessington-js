eb005dc01de44af25e3c0093fceca489fdfe8906
Suwanna Truss <suwanna.truss@softwire.com>
2021-06-01 16:45:21 +0100

    Red-6 Obtained availableMoves for Knights

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,20 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        const currentRow = location.row;
+        const currentCol = location.col;
+        const availableMoves = [];
+
+        availableMoves.push(Square.at(currentRow + 2, currentCol + 1));
+        availableMoves.push(Square.at(currentRow + 2, currentCol - 1));
+        availableMoves.push(Square.at(currentRow - 2, currentCol + 1));
+        availableMoves.push(Square.at(currentRow - 2, currentCol - 1));
+        availableMoves.push(Square.at(currentRow + 1, currentCol + 2));
+        availableMoves.push(Square.at(currentRow - 1, currentCol + 2));
+        availableMoves.push(Square.at(currentRow + 1, currentCol - 2));
+        availableMoves.push(Square.at(currentRow - 1, currentCol - 2));
+
+        return availableMoves;
     }
 }
