1c90376d1c22481fa93398f0cd14d0c88fd2c0ab
Suwanna Truss <suwanna.truss@softwire.com>
2021-06-01 17:19:30 +0100

    Red-7 Obtained Available moves for King

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,36 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        const currentRow = location.row;
+        const currentCol = location.col;
+        const availableMoves = [];
+
+        availableMoves.push(Square.at(currentRow + 1, currentCol));
+        availableMoves.push(Square.at(currentRow + 1, currentCol + 1));
+        availableMoves.push(Square.at(currentRow + 1, currentCol - 1));
+        availableMoves.push(Square.at(currentRow, currentCol + 1));
+        availableMoves.push(Square.at(currentRow, currentCol - 1));
+        availableMoves.push(Square.at(currentRow - 1, currentCol - 1));
+        availableMoves.push(Square.at(currentRow - 1, currentCol));
+        availableMoves.push(Square.at(currentRow - 1, currentCol + 1));
+
+        // for (let r = -1; r <=1; r++) {
+        //     for (let c = -1; c <= 1; c++) {
+        //         let currentSquare = new Square.at(currentRow + r, currentCol + c)
+        //         console.log(currentSquare)
+        //         console.log(location)
+        //         console.log(currentSquare !== location)
+        //         if (currentSquare.row !== location.row && currentSquare.col !== location.col) {
+        //         // if (c !== 0 && r !== 0) {
+        //             availableMoves.push(currentSquare);
+
+        //             // availableMoves.push(Square.at(currentRow + r, currentCol + c));
+        //         // }
+        //         }
+        //     }
+        // }
+
+        return availableMoves;
     }
 }
