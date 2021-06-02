b7a1572b6d8de4519b6f5a295a7fdf9dbe97b816
Ilyas Ali <Ilyas.Ali@softwire.com>
2021-06-02 17:48:34 +0100

    Red-11 Queen cannot move past other pieces

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -12,33 +12,59 @@ export default class Queen extends Piece {
         const currentRow = location.row;
         const currentCol = location.col;
 
-        for (let i = 1; i < 8; i++) {
-            //lateral
-            if (currentRow + i < 8) {
-                availableMoves.push(Square.at(currentRow + i, currentCol));
-            }
-            if (currentRow - i >= 0) {
-                availableMoves.push(Square.at(currentRow - i, currentCol));
-            }
-            if (currentCol + i < 8) {
-                availableMoves.push(Square.at(currentRow, currentCol + i));
-            }
-            if (currentCol - i >= 0) {
-                availableMoves.push(Square.at(currentRow, currentCol - i));
-            }
+        let stopTopRight = false;
+        let stopTopLeft = false;
+        let stopDownRight = false;
+        let stopDownLeft = false;
+        let stopRight = false;
+        let stopLeft = false;
+        let stopUp = false;
+        let stopDown = false;
 
-            //Diagonal
-            if (currentRow + i < 8 && currentCol + i < 8) {
-                availableMoves.push(Square.at(currentRow + i, currentCol + i));
-            }
-            if (currentRow - i >= 0 && currentCol + i < 8) {
-                availableMoves.push(Square.at(currentRow - i, currentCol + i));
-            }
-            if (currentRow + i < 8 && currentCol - i >= 0) {
-                availableMoves.push(Square.at(currentRow + i, currentCol - i));
-            }
-            if (currentRow - i >= 0 && currentCol - i >= 0) {
-                availableMoves.push(Square.at(currentRow - i, currentCol - i));
+        for (let i = 1; i < 8; i++) {
+            getNextAvailableMove(currentRow + i, currentCol + i, stopTopRight, "topRight");
+            getNextAvailableMove(currentRow - i, currentCol + i, stopDownRight, "downRight");
+            getNextAvailableMove(currentRow + i, currentCol - i, stopTopLeft, "topLeft");
+            getNextAvailableMove(currentRow - i, currentCol - i, stopDownLeft, "downLeft");
+            getNextAvailableMove(currentRow + i, currentCol, stopUp, "up");
+            getNextAvailableMove(currentRow - i, currentCol, stopDown, "down");
+            getNextAvailableMove(currentRow, currentCol + i, stopRight, "right");
+            getNextAvailableMove(currentRow, currentCol - i, stopLeft, "left");
+        }
+
+        function getNextAvailableMove(newRow, newCol, check, direction) {
+            if (newRow < 8 && newRow >= 0 && newCol < 8 && newCol >= 0 && !check) {
+                const nextMove = Square.at(newRow, newCol);
+                if (!board.getPiece(nextMove)) {
+                    availableMoves.push(nextMove)
+                } else {
+                    switch (direction) {  
+                        case "topRight":
+                            stopTopRight = true;
+                            break;
+                        case "downRight":
+                            stopDownRight = true;
+                            break;
+                        case "topLeft":
+                            stopTopLeft = true;
+                            break;
+                        case "downLeft":
+                            stopDownLeft = true;
+                            break;
+                        case "up":
+                            stopUp = true;
+                            break;
+                        case "down":
+                            stopDown = true;
+                            break;
+                        case "right":
+                            stopRight = true;
+                            break;
+                        case "left":
+                            stopLeft = true;
+                            break;
+                    }
+                }
             }
         }
         return availableMoves;
