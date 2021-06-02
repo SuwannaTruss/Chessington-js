4e93d3eadba8878548d3aa70032af1e568b69f81
Suwanna Truss <suwanna.truss@softwire.com>
2021-06-02 15:57:02 +0100

    end of day2

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -10,54 +10,86 @@ export default class Rook extends Piece {
         const location = board.findPiece(this);
         const currentRow = location.row;
         const currentCol = location.col;
-        let availableMoves = [];
+        const availableMoves = [];
 
         let stopRight = false;
         let stopLeft = false;
         let stopUp = false;
         let stopDown = false;
-        let nextMove;
 
         for (let i = 1; i < 8; i++) {
-            if (currentRow + i < 8 && !stopUp) {
-                nextMove = Square.at(currentRow + i, currentCol);
-                if (!board.getPiece(nextMove)) {
-                    availableMoves.push(nextMove)
-                } else {
-                    stopUp = true;
-                }
-            }
-            if (currentRow - i >= 0 && !stopDown) {
-                nextMove = Square.at(currentRow - i, currentCol);
-                if (!board.getPiece(nextMove)) {
-                    availableMoves.push(nextMove)
-                } else {
-                    stopDown = true;
-                }
-            }
-            if (currentCol + i < 8 && !stopRight) {
-                nextMove = Square.at(currentRow, currentCol + i);
-                if (!board.getPiece(nextMove)) {
-                    availableMoves.push(nextMove)
-                } else {
-                    stopRight = true;
-                }
-            }
-            if (currentCol - i >= 0 && !stopLeft) {
-                nextMove = Square.at(currentRow, currentCol - i);
-                if (!board.getPiece(nextMove)) {
-                    availableMoves.push(nextMove)
-                } else {
-                    stopLeft = true;
-                }
-            }
+            getNextAvailableMove(currentRow + i, currentCol, stopUp);
+            getNextAvailableMove(currentRow - i, currentCol, stopDown);
+            getNextAvailableMove(currentRow, currentCol + i, stopRight);
+            getNextAvailableMove(currentRow, currentCol - i, stopLeft);
         }
 
-        // const filterAvailableMoves = availableMoves.filter(square => !board.getPiece(square));
-        // availableMoves.forEach(square => console.log(!!board.getPiece(square)));
+        function getNextAvailableMove(newRow, newCol, check) {
+            console.log(`${newRow}, ${newCol}, ${check}`);
+            debugger;
+            if (newRow < 8 && newRow >= 0 && newCol < 8 && newCol >= 0 && !check) {
+                const nextMove = Square.at(newRow, newCol);
+                if (!board.getPiece(nextMove)) {
+                    availableMoves.push(nextMove);
+                    console.log(availableMoves)
+                } else {
+                    check = true;
+                }
+            }
 
-        // console.log({filterAvailableMoves});
-        
+        }
+
+        //     let newRow = currentRow - i;     
+        //     let newCol = currentCol;
+        //     let check = stopDown;    
+        //     if (newRow < 8 && newRow >= 0 && newCol < 8 && newCol >= 0 && !check) {
+        //         const nextMove = Square.at(newRow, newCol);
+        //         if (!board.getPiece(nextMove)) {
+        //             availableMoves.push(nextMove);
+        //         } else {
+        //             check = true;
+        //         }
+        //     }
+
+        //     let newRow = currentRow;     
+        //     let newCol = currentCol + i;
+        //     let check = stopRight;    
+        //     if (newRow < 8 && newRow >= 0 && newCol < 8 && newCol >= 0 && !check) {
+        //         const nextMove = Square.at(newRow, newCol);
+        //         if (!board.getPiece(nextMove)) {
+        //             availableMoves.push(nextMove);
+        //         } else {
+        //             check = true;
+        //         }
+        //     }
+
+
+        //     if (currentRow - i >= 0 && !stopDown) {
+        //         const nextMove = Square.at(currentRow - i, currentCol);
+        //         if (!board.getPiece(nextMove)) {
+        //             availableMoves.push(nextMove);
+        //         } else {
+        //             stopDown = true;
+        //         }
+        //     }
+        //     if (currentCol + i < 8 && !stopRight) {
+        //         const nextMove = Square.at(currentRow, currentCol + i);
+        //         if (!board.getPiece(nextMove)) {
+        //             availableMoves.push(nextMove);
+        //         } else {
+        //             stopRight = true;
+        //         }
+        //     }
+        //     if (currentCol - i >= 0 && !stopLeft) {
+        //         const nextMove = Square.at(currentRow, currentCol - i);
+        //         if (!board.getPiece(nextMove)) {
+        //             availableMoves.push(nextMove);
+        //         } else {
+        //             stopLeft = true;
+        //         }
+        //     }
+        // }
         return availableMoves;
     }
 }
+
