15dd31051bf3d269ed9bd16071318f204d600d0c
Suwanna Truss <suwanna.truss@softwire.com>
2021-06-02 14:32:38 +0100

    Red-9 rook.js cannot move through pieces

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -10,22 +10,54 @@ export default class Rook extends Piece {
         const location = board.findPiece(this);
         const currentRow = location.row;
         const currentCol = location.col;
-        const availableMoves = [];
+        let availableMoves = [];
 
-        for (let i = 1; i < 8; i ++) {
-            if (currentRow + i < 8) {
-                availableMoves.push(Square.at(currentRow + i, currentCol))
+        let stopRight = false;
+        let stopLeft = false;
+        let stopUp = false;
+        let stopDown = false;
+        let nextMove;
+
+        for (let i = 1; i < 8; i++) {
+            if (currentRow + i < 8 && !stopUp) {
+                nextMove = Square.at(currentRow + i, currentCol);
+                if (!board.getPiece(nextMove)) {
+                    availableMoves.push(nextMove)
+                } else {
+                    stopUp = true;
+                }
             }
-            if (currentRow - i >= 0) {
-                availableMoves.push(Square.at(currentRow - i, currentCol))
+            if (currentRow - i >= 0 && !stopDown) {
+                nextMove = Square.at(currentRow - i, currentCol);
+                if (!board.getPiece(nextMove)) {
+                    availableMoves.push(nextMove)
+                } else {
+                    stopDown = true;
+                }
             }
-            if (currentCol + i < 8) {
-                availableMoves.push(Square.at(currentRow, currentCol + i))
+            if (currentCol + i < 8 && !stopRight) {
+                nextMove = Square.at(currentRow, currentCol + i);
+                if (!board.getPiece(nextMove)) {
+                    availableMoves.push(nextMove)
+                } else {
+                    stopRight = true;
+                }
             }
-            if (currentCol - i >= 0) {
-                availableMoves.push(Square.at(currentRow, currentCol - i))
+            if (currentCol - i >= 0 && !stopLeft) {
+                nextMove = Square.at(currentRow, currentCol - i);
+                if (!board.getPiece(nextMove)) {
+                    availableMoves.push(nextMove)
+                } else {
+                    stopLeft = true;
+                }
             }
         }
+
+        // const filterAvailableMoves = availableMoves.filter(square => !board.getPiece(square));
+        // availableMoves.forEach(square => console.log(!!board.getPiece(square)));
+
+        // console.log({filterAvailableMoves});
+        
         return availableMoves;
     }
 }
