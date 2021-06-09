4cb23d46f649bfb7162cde8c65af98728a263aa6
Oskar Williams <oskarwilliams378@gmail.com>
2021-06-09 13:01:46 +0100

    Add Rook blocking by King

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -9,32 +9,35 @@ export default class Piece {
         const availableMoves = [];
         const isOccupied = [];
         const location = board.findPiece(this);
-     
+
         directions.forEach(direction => {
             for (let i = 1; i < count; i++) {
                 const nextMove = Square.at(location.row + i * direction.x, location.col + i * direction.y);
-                
+
                 if (nextMove.row < 8 && nextMove.row >= 0 && nextMove.col < 8 && nextMove.col >= 0) {
-                    if (!board.getPiece(nextMove)) {
+                    const occupiedPiece = board.getPiece(nextMove);
+
+                    if (!occupiedPiece) {
                         availableMoves.push(nextMove);
-                    } else {
-                        //break;
-                         const occupiedPiece = board.getPiece(nextMove);   
-                if (Object.is(this.player, occupiedPiece.player)) {
-                    break
-                } else { // : check if it is king ? add to availableMoves :
-                        isOccupied.push(nextMove)
-                    }
+                    } else { // check if it is the same player, if so break
+                        if (occupiedPiece.player === this.player) {
+                            break
+                        } // check if it is king, if not add to availableMoves
+                        if (occupiedPiece.constructor.name === "King") {
+                            break;
+                        }
+                        availableMoves.push(nextMove);
+                        break;
                     }
                 }
-                        // console.log(this.player) // Symbol(white)
-                        // console.log(occupiedPiece.player)
-                        // console.log(Object.keys(this)) // ['player']
-                        // console.log(Object.values(this)) // [Symbol(white)]
-                        // console.log(Object.is(this.player, occupiedPiece.player))
-                        // console.log(this.player === occupiedPiece)
-                        // isOccupied.push(nextMove); 
             }
+            // console.log(this.player) // Symbol(white)
+            // console.log(occupiedPiece.player)
+            // console.log(Object.keys(this)) // ['player']
+            // console.log(Object.values(this)) // [Symbol(white)]
+            // console.log(Object.is(this.player, occupiedPiece.player))
+            // console.log(this.player === occupiedPiece)
+            // isOccupied.push(nextMove); 
         });
 
         return availableMoves;
